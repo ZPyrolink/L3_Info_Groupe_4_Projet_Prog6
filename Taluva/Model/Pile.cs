7798393ddd1e91010d441b6b09b99dc39a170158
Arromatizey <chedadblackmed41@gmail.com>
2023-05-03 15:40:55 +0200

    Ajout de la pioche creation d'une pile aleatoire à partir d'un tableau de tuile passer en parametre creation d'une methode 'piocher'

---
+++ Taluva/Model/Pile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+namespace Taluva.Model;
+
+public class Pile<T>
+{
+    private Stack<T> pile;
+    private List<T> played;
+
+    public Pile(T[] list)
+    {
+        Random random = new Random();
+        list = list.OrderBy(x => random.Next()).ToArray();
+        for (int i = 0; i < list.Length; i++)
+        {
+            pile.Push(list[i]);
+        }
+        played = new List<T>();
+    }
+
+    public T draw()
+    {
+        T c = pile.Pop();
+        played.Add(c);
+        return c;
+    }
+}
