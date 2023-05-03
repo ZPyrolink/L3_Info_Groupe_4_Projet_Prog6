42ad78fe06c6e65f608a3e421580f9b97ba422e5
Corenthin Z <38cz74@gmail.com>
2023-05-03 23:30:53 +0200

    Update UI and add debug feature

--- Assets/Scripts/UI/UiMgr.cs
+++ Assets/Scripts/UI/UiMgr.cs
@@ -1,3 +1,7 @@
+using System;
+
+using Taluva.Model;
+
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,14 +9,13 @@ using UnityUtils.GameObjects;
 
 using Utils;
 
+using Random = UnityEngine.Random;
+
 namespace UI
 {
     public class UiMgr : MonoBehaviour
     {
-        [SerializeField]
-        private GameObject light;
-        
-        [System.Serializable]
+        [Serializable]
         public class Player
         {
             [SerializeField]
@@ -41,35 +44,88 @@ namespace UI
         [SerializeField]
         private Player[] players =
         {
-            new("P1", ColorUtils.FromInt(0xFFD20F28), new[] { 0, 1, 2 }),
-            new("P2", ColorUtils.FromInt(0xFF6C935E), new[] { 1, 1, 2 }),
-            new("P3", ColorUtils.FromInt(0xFF4855B7), new[] { 2, 1, 2 }),
-            new("P4", ColorUtils.FromInt(0xFFFFD700), new[] { 3, 1, 2 })
+            new("P1", ColorUtils.FromInt((int) PlayerColor.Red), new[] { 0, 1, 2 }),
+            new("P2", ColorUtils.FromInt((int) PlayerColor.Green), new[] { 1, 1, 2 }),
+            new("P3", ColorUtils.FromInt((int) PlayerColor.Blue), new[] { 2, 1, 2 }),
+            new("P4", ColorUtils.FromInt((int) PlayerColor.Yellow), new[] { 3, 1, 2 })
         };
 
         [SerializeField]
         private int currentPlayerIndex;
+
+        public int CurrentPlayerIndex
+        {
+            get => currentPlayerIndex;
+            set => currentPlayerIndex = value >= players.Length ? 0 : value;
+        }
 
         private Player CurrentPlayer => players[currentPlayerIndex];
 
         [SerializeField]
         private GameObject playerPrefab;
 
-        private GameObject[] guis = new GameObject[4];
+        private readonly GameObject[] guis = new GameObject[4];
 
         [SerializeField]
         private Text[] currentPlayerBuild;
 
-        private void NextPlayer()
+        [SerializeField]
+        private GameObject tile, builds;
+
+        [Header("Debug keys")]
+        [SerializeField]
+        private KeyCode phase1 = KeyCode.Keypad1;
+
+        [SerializeField]
+        private KeyCode phase2 = KeyCode.Keypad2,
+            nextPlayer = KeyCode.KeypadEnter,
+            nextPhase = KeyCode.Return;
+
+        private byte _phase;
+        private byte Phase
         {
-            if (++currentPlayerIndex >= players.Length)
-                currentPlayerIndex = 0;
+            get => _phase;
+            set
+            {
+                if (value > 2)
+                {
+                    NextPlayer();
+                    _phase = 1;
+                }
+                else
+                {
+                    _phase = value;
+                }
+
+                (_phase switch
+                {
+                    1 => Phase1,
+                    2 => Phase2,
+                    _ => (Action) null
+                })?.Invoke();
+            }
+        }
+
+        #region Unity events
+
+        private void Start()
+        {
+            Phase = 1;
         }
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(nextPlayer))
                 NextPlayer();
+
+            if (Input.GetKeyDown(nextPhase))
+                Next();
+
+            if (Input.GetKeyDown(phase1))
+                Phase = 1;
+
+            if (Input.GetKeyDown(phase2))
+                Phase = 2;
         }
 
         // Start is called before the first frame update
@@ -96,10 +152,33 @@ namespace UI
                 rt.anchorMax = Vector2.one;
                 rt.anchoredPosition = new(-10, -10 - 110 * i);
             }
-            
+
             currentPlayerBuild[0].text = CurrentPlayer.Builds[0].ToString();
             currentPlayerBuild[1].text = CurrentPlayer.Builds[1].ToString();
             currentPlayerBuild[2].text = CurrentPlayer.Builds[2].ToString();
         }
+
+        #endregion
+
+        public void Next() => Phase++;
+
+        private void Phase1()
+        {
+            int[] values = (int[]) Enum.GetValues(typeof(BiomeColor));
+            MeshRenderer mr = tile.transform.GetComponentInChildren<MeshRenderer>();
+            mr.materials[1].color = ColorUtils.FromInt(values[Random.Range(0, values.Length - 1)]);
+            mr.materials[2].color = ColorUtils.FromInt(values[Random.Range(0, values.Length - 1)]);
+            mr.materials[3].color = ColorUtils.FromInt(values[Random.Range(0, values.Length - 1)]);
+            tile.SetActive(true);
+            builds.SetActive(false);
+        }
+
+        private void Phase2()
+        {
+            tile.SetActive(false);
+            builds.SetActive(true);
+        }
+
+        private void NextPlayer() => CurrentPlayerIndex++;
     }
 }
