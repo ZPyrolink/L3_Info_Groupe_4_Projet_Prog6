7c6b3e454126fbada36979e7454804d3e0766201
Corenthin Z <38cz74@gmail.com>
2023-05-05 08:56:42 +0200

    Update Tests

--- Packages/TestsTaluva/TestDynamicMatrix.cs
+++ Packages/TestsTaluva/TestDynamicMatrix.cs
@@ -1,53 +1,56 @@
-// using Taluva.Utils;
-// using System.Drawing;
-//
-// namespace TestDynamicMatrix
-// {
-//     [TestClass]
-//     public class TestDynamicMatrix
-//     {
-//         private DynamicMatrix<String> matrix;
-//
-//         [TestInitialize]
-//         public void Init()
-//         {
-//             matrix = new DynamicMatrix<String>();
-//             matrix.Add("A", new Point(0, 0));
-//             matrix.Add("B", new Point(0, 1));
-//             matrix.Add("C", new Point(1, 0));
-//         }
-//
-//         [TestMethod]
-//         public void TestAjout()
-//         {
-//             Assert.AreEqual("A", matrix.GetValue(new Point(0, 0)));
-//             Assert.AreEqual("B", matrix.GetValue(new Point(0, 1)));
-//             Assert.AreEqual("C", matrix.GetValue(new Point(1, 0)));
-//         }
-//
-//         [TestMethod]
-//         public void TestVoid()
-//         {
-//             Assert.IsFalse(matrix.IsVoid(new Point(0, 0)));
-//             Assert.IsFalse(matrix.IsVoid(new Point(0, 1)));
-//             Assert.IsFalse(matrix.IsVoid(new Point(1, 0)));
-//
-//             Assert.IsTrue(matrix.IsVoid(new Point(2, 0)));
-//             Assert.IsTrue(matrix.IsVoid(new Point(0, 2)));
-//             Assert.IsTrue(matrix.IsVoid(new Point(2, 2)));
-//         }
-//
-//         [TestMethod]
-//         public void TestReplace()
-//         {
-//             matrix.Add("L", new Point(0, 0));
-//             matrix.Add("K", new Point(1, 0));
-//
-//             Assert.AreNotEqual("A", matrix.GetValue(new Point(0, 0)));
-//             Assert.AreNotEqual("C", matrix.GetValue(new Point(1, 0)));
-//
-//             Assert.AreEqual("L", matrix.GetValue(new Point(0, 0)));
-//             Assert.AreEqual("K", matrix.GetValue(new Point(1, 0)));
-//         }
-//     }
-// }
+using NUnit.Framework;
+
+using Taluva.Utils;
+
+namespace TestsTaluva
+{
+    [TestFixture]
+    public class TestDynamicMatrix
+    {
+        private DynamicMatrix<string> _matrix;
+
+        [SetUp]
+        public void Init()
+        {
+            _matrix = new()
+            {
+                { "A", new(0, 0) },
+                { "B", new(0, 1) },
+                { "C", new(1, 0) }
+            };
+        }
+
+        [Test]
+        public void TestAjout()
+        {
+            Assert.AreEqual("A", _matrix.GetValue(new(0, 0)));
+            Assert.AreEqual("B", _matrix.GetValue(new(0, 1)));
+            Assert.AreEqual("C", _matrix.GetValue(new(1, 0)));
+        }
+
+        [Test]
+        public void TestVoid()
+        {
+            Assert.IsFalse(_matrix.IsVoid(new(0, 0)));
+            Assert.IsFalse(_matrix.IsVoid(new(0, 1)));
+            Assert.IsFalse(_matrix.IsVoid(new(1, 0)));
+
+            Assert.IsTrue(_matrix.IsVoid(new(2, 0)));
+            Assert.IsTrue(_matrix.IsVoid(new(0, 2)));
+            Assert.IsTrue(_matrix.IsVoid(new(2, 2)));
+        }
+
+        [Test]
+        public void TestReplace()
+        {
+            _matrix.Add("L", new(0, 0));
+            _matrix.Add("K", new(1, 0));
+
+            Assert.AreNotEqual("A", _matrix.GetValue(new(0, 0)));
+            Assert.AreNotEqual("C", _matrix.GetValue(new(1, 0)));
+
+            Assert.AreEqual("L", _matrix.GetValue(new(0, 0)));
+            Assert.AreEqual("K", _matrix.GetValue(new(1, 0)));
+        }
+    }
+}
