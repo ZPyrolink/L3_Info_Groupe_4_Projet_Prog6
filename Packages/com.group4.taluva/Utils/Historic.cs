74f39b90f1cac58df1d89222cc114c59d5aae9c1
Corenthin Z <38cz74@gmail.com>
2023-05-08 15:56:14 +0200

    Create Historic class

---
+++ Packages/com.group4.taluva/Utils/Historic.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taluva.Utils
+{
+    public class Historic<T> : List<T>
+    {
+        public int Index { get; private set; } = -1;
+
+        public Historic() { }
+
+        public Historic(Historic<T> original, int currentIndex) : base(original)
+        {
+            Index = currentIndex;
+        }
+
+        public bool CanUndo => Index != -1;
+        public bool CanRedo => Index != Count;
+
+        public new void Add(T element)
+        {
+            while (CanRedo)
+                RemoveAt(Index + 1);
+
+            Index++;
+            base.Add(element);
+        }
+
+        public T Undo()
+        {
+            if (Count == 0)
+                throw new InvalidOperationException();
+
+            return this[Index--];
+        }
+
+        public T Redo()
+        {
+            if (Index == Count - 1)
+                throw new InvalidOperationException();
+
+            return this[++Index];
+        }
+
+        public T Last => this[Index];
+        public T Next => this[Index + 1];
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+            for (int i = 0; i < Count; i++)
+            {
+                T t = this[i];
+                if (i == Index)
+                    sb.Append('*');
+                sb.Append(t).Append('\n');
+            }
+
+            return sb.ToString();
+        }
+    }
+}
