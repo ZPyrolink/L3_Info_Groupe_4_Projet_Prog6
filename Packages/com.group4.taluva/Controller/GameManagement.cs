87cc17e4b73f06cb7d8419671ef86f5374f5aa00
goninf <gonin.flavien@gmail.com>
2023-05-09 10:19:51 +0200

    J'ai bien l'historic

--- Packages/com.group4.taluva/Controller/GameManagement.cs
+++ Packages/com.group4.taluva/Controller/GameManagement.cs
@@ -9,7 +9,7 @@ namespace Taluva.Controller
     public class GameManagment
     {
         private Player[] players;
-        private Player actualPlayer { get; }
+        private Player actualPlayer;
         public int NbPlayers { get; set; }
         private int actualTurn;
         private int maxTurn;
@@ -31,50 +31,69 @@ namespace Taluva.Controller
 
         private class Coup
         {
-            //New Chunk
-            public Vector2Int position;
-            public Rotation rotation;
+            //New Chunk or cells
+            public Vector2Int[] positions;
+            public Rotation? rotation;
 
             //Last Chunk or last cells
             public Cell[] cells;
+            public Chunk chunk;
             public Player player;
 
             //Use it when there is nothing at the position
-            public Coup(Vector2Int position, Rotation rotation)
+            public Coup(Vector2Int[] positions, Rotation? rotation, Player actualPlayer)
             {
-                this.position = position;
+                this.positions = positions;
                 this.rotation = rotation;
                 this.player = actualPlayer;
             }
 
-            public Coup(Vector2Int position, Rotation rotation, Chunk cells) : this(position, rotation)
+            public Coup(Vector2Int[] positions, Rotation rotation, Player actualPlayer, Chunk chunk) : this(positions, rotation, actualPlayer)
             {
-                this.cells = (Cell[])cells.Coords.Clone();
+                this.chunk = chunk;
             }
 
-            public Coup(Vector2Int position, Rotation rotation, Cell[] cells) : this(position, rotation)
+            public Coup(Vector2Int[] positions, Rotation? rotation, Player actualPlayer, Cell[] cells) : this(positions, rotation, actualPlayer)
             {
                 this.cells = cells;
             }
         }
 
+        public void AddHistoric(Vector2Int[] positions, Rotation rotation)
+        {
+            historic.Add(new(positions, rotation, actualPlayer));
+        }
+
+        public void AddHistoric(Vector2Int position, Rotation rotation, Chunk chunk)
+        {
+            historic.Add(new(new[] { position }, rotation, actualPlayer, chunk));
+        }
+
+        public void AddHistoric(Vector2Int[] positions, Cell[] cells)
+        {
+            historic.Add(new(positions, null, actualPlayer, cells));
+        }
+
         public void Undo()
         {
+            //ne pas oublier le changement de player
             Coup c = historic.Undo();
-            Chunk chunk = gameBoard.WorldMap.GetValue(c.position).ParentCunk;
-            if (c.cells == null) {
-                gameBoard.RemoveChunk(chunk);
-                return;
+            if(c.chunk == null && c.cells == null) {
+                foreach (Vector2Int position in c.positions)
+                    gameBoard.WorldMap.Remove(position);
             }
-
-            gameBoard.AddChunk(chunk, c.player, new(c.position), c.rotation);
         }
 
         public void Redo()
         {
             Coup c = historic.Redo();
-            Chunk chunk = new(c.cells[1], c.cells[2]);
-            gameBoard.AddChunk(chunk, c.player, new(c.position), c.rotation);
+            if(c.chunk == null) {
+                for(int i = 0; i < c.cells.Length; i++) {
+                    gameBoard.WorldMap.Add(c.cells[i], c.positions[i]);
+                }
+            } else {
+                gameBoard.AddChunk(c.chunk, c.player, new(c.positions[0]), c.rotation);
+            }
         }
 
         public Player? GetWinner()
