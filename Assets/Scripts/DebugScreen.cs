9a5391135253e82b4b4b630ab6a8b8de0aa7b891
Corenthin Z <38cz74@gmail.com>
2023-05-10 22:00:29 +0200

    Update Debug

--- Assets/Scripts/DebugScreen.cs
+++ Assets/Scripts/DebugScreen.cs
@@ -3,6 +3,7 @@
 using Taluva.Model;
 
 using UnityEngine;
+using UnityEngine.Profiling;
 using UnityEngine.UI;
 
 public class DebugScreen : MonoBehaviour
@@ -20,6 +21,9 @@ public class DebugScreen : MonoBehaviour
 
     private void Start()
     {
+#if !(DEVELOPMENT_BUILD || UNITY_EDITOR)
+        Destroy(this);
+#endif
         Debug.Log("Debug enabled!");
     }
 
@@ -29,8 +33,12 @@ public class DebugScreen : MonoBehaviour
             gui.SetActive(!gui.activeSelf);
 
         _dt += (Time.deltaTime - _dt) * 0.1f;
-        fps.text = $@"{Mathf.Ceil(1.0f / _dt)} fps";
-        
-        
+        fps.text = $@"{Mathf.Ceil(1.0f / _dt)} fps
+{Profiler.usedHeapSizeLong} / {Profiler.maxUsedMemory:e} o allocated by Unity
+{Profiler.GetAllocatedMemoryForGraphicsDriver():e} allocated for graphics driver
+{Profiler.GetMonoUsedSizeLong():e} / {Profiler.GetMonoHeapSizeLong():e} o allocated managed-memory
+{Profiler.GetTempAllocatorSize():e} o temporary allocated
+{Profiler.GetTotalReservedMemoryLong():e} o total reserved memory
+";
     }
 }
