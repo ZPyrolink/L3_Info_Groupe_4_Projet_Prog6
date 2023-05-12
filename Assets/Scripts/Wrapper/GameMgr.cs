173a1330b1b896dbadcc2ba8d61ba455896d9e7e
Corenthin Z <38cz74@gmail.com>
2023-05-12 16:39:17 +0200

    Begin notification system

--- Assets/Scripts/Wrapper/GameMgr.cs
+++ Assets/Scripts/Wrapper/GameMgr.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 
 using Taluva.Controller;
+using Taluva.Model;
+
+using UI;
 
 using UnityEngine;
 
@@ -10,13 +13,40 @@ namespace Wrapper
     {
         [SerializeField]
         private int nbPlayers;
-        
+
         public static GameManagment Instance;
 
         private void Awake()
         {
             Instance = new(nbPlayers);
+        }
+
+        private void Start()
+        {
+            SetHandlers();
             Instance.InitPlay();
+        }
+
+        private void SetHandlers()
+        {
+            Instance.ChangePhase = phase =>
+            {
+                UiMgr ui = UiMgr.Instance;
+
+                (phase switch
+                {
+                    TurnPhase.SelectCells => (Action) ui.Phase1,
+                    TurnPhase.PlaceBuilding => ui.Phase2,
+                    _ => () => Debug.LogWarning($"The {Instance.actualPhase} is not implemented!")
+                }).Invoke();
+            };
+
+            Instance.NotifyEndGame = (player, end) => { Debug.Log(player + " " + end); };
+
+            Instance.NotifyPlayerEliminated = player => { Debug.Log(player); };
+
+            Instance.NotifyAIBuildingPlacement = (building, i) => throw new NotImplementedException();
+            Instance.NotifyAIChunkPlacement = rotation => throw new NotImplementedException();
         }
     }
 }
