d9586433cf731c88e50bb74ab9203a6e70f2a6b9
KaTSukiFireStar <gonin.flavien@gmail.com>
2023-05-17 11:42:48 +0200

    Ajout VictoryMgr

---
+++ Assets/Scripts/UI/VictoryMgr.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utils;
+
+namespace UI
+{
+    public class VictoryMgr : MonoBehaviourMgr<VictoryMgr>
+    {
+        public void Start()
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        public void Restart() => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        public void MainMenu() => throw new NotImplementedException();
+
+        public void Quit() =>
+#if UNITY_EDITOR
+        Debug.Break();
+#else
+        Application.Quit();
+#endif
+
+    }
+}
