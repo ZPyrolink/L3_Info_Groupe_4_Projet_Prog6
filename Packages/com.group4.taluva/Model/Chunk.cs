b0b0461c622cf31d57d72934ef6d110ce8a0a6dd
mohammadhajj <mhamadhajj02@gmail.com>
2023-05-22 16:52:14 +0200

    Copie des cells

--- Packages/com.group4.taluva/Model/Chunk.cs
+++ Packages/com.group4.taluva/Model/Chunk.cs
@@ -29,11 +29,15 @@ namespace Taluva.Model
         public Chunk(Chunk c)
         {
             Coords = new Cell[3];
-            Coords[0] = c.Coords[0];
-            Coords[1] = c.Coords[1];
-            c.Coords[1].ParentChunk = c;
-            Coords[2] = c.Coords[2];
-            c.Coords[2].ParentChunk = c;
+            Coords[0] = new(Biomes.Volcano, this);
+            Coords[1] = new Cell(c.Coords[1].ActualBiome);
+            Coords[1].Owner = c.Coords[1].Owner;
+            Coords[1].ActualBuildings = c.Coords[1].ActualBuildings;
+            Coords[1].ParentChunk = this;
+            Coords[2] = new Cell(c.Coords[2].ActualBiome);
+            Coords[2].Owner = c.Coords[2].Owner;
+            Coords[2].ActualBuildings = c.Coords[2].ActualBuildings;
+            Coords[2].ParentChunk = this;
             Level = c.Level;
         }
 
